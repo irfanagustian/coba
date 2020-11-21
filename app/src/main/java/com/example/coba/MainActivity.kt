b9a2b0f84ca56e08fabc1e7d61ae843762c20e91
ipan <irfanagustian0608@gmail.com>
2020-11-21 17:32:21 +0700

    aaaaaaaaa

--- app/src/main/java/com/example/coba/MainActivity.kt
+++ app/src/main/java/com/example/coba/MainActivity.kt
@@ -1,19 +1,47 @@
 package com.example.coba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button1 = findViewById<Button>(R.id.btn1) as Button
 
-        button1.setOnClickListener{
-            Toast.makeText(this@MainActivity, "I Love You", Toast.LENGTH_SHORT).show()
-        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(onBottomNavigationListener)
+        var fragment = supportFragmentManager.beginTransaction()
+        fragment.add(R.id.fl, HomeFragment())
+        fragment.commit()
+
     }
+
+    private val onBottomNavigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var selectedFragment : Fragment = HomeFragment()
+
+        when(item.itemId) {
+            R.id.id_home -> {
+                selectedFragment = HomeFragment()
+            }
+            R.id.id_profil -> {
+                selectedFragment =ProfilFragment()
+            }
+        }
+
+        var fragment = supportFragmentManager.beginTransaction()
+        fragment.replace(R.id.fl, selectedFragment)
+        fragment.commit()
+        true
+
+    }
+
 }
---
+++ app/src/main/java/com/example/coba/MasukActivity.kt
@@ -0,0 +1,14 @@
+package com.example.coba
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+public class MasukActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_masuk)
+
+    }
+
+}
